9e869ab8abcee753cab61e8c630598640df0654d
Sahilx8 <149781855+Sahilx8@users.noreply.github.com>
2024-10-22 08:01:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PDFUploader from './FileUpload';
-import { BrowserRouter } from 'react-router-dom'
 import Summary from './Summary';
 
 function App() {
   return (
-    <Routes>
-    <Route path="/" element={<PDFUploader />} />
-    <Route path="/summary" element={<Summary />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PDFUploader />} />
+        <Route path="/summary" element={<Summary />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
